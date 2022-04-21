6368ee6971d4fa33d1ca795f3b0fa63fb8e0fe30
simonpai <simon.pai@gmail.com>
2022-04-21 11:47:53 +0800

    [#26] Rename ui-elements -> client-sdk-ui

--- packages/client-sdk/src/with-ui.js
+++ packages/client-sdk/src/with-ui.js
@@ -1,5 +1,5 @@
 import MisoClient from '.';
-import { UiPlugin } from '@miso.ai/ui-elements';
+import { UiPlugin } from '@miso.ai/client-sdk-ui';
 
 // Turn on directly, so users don't need to put down a script tag to do so.
 MisoClient.plugins.use(UiPlugin);
