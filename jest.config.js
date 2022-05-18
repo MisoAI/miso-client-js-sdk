fd830bdeb12a685822bbc88e81ef3e37b7b979a7
simonpai <simon.pai@gmail.com>
2022-05-18 15:48:17 +0800

    Fix and simplify unit.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 module.exports = {
   verbose: true,
   testEnvironment: 'node',
   globals: {
     NODE_ENV: 'test'
   },
+  testMatch: [
+    path.join(__dirname, 'packages/**/*.test.js')
+  ],
   transform: {
     '^.+\\.js$': 'babel-jest'
   }
