bd455e1aba4055266477be98e7535b836b8f0265
simonpai <simon.pai@gmail.com>
2022-05-23 14:39:43 +0800

    [#38] Documentation: use computed data for region variable.

--- doc/_data/eleventyComputed.js
+++ doc/_data/eleventyComputed.js
@@ -1,8 +1,17 @@
+function region(data) {
+  return urlSegment(data, 0);
+}
+
 function section(data) {
+  return urlSegment(data, 1);
+}
+
+function urlSegment(data, index) {
   const pageUrl = data.page.url.substring(1);
-  return (pageUrl && pageUrl.split('/')[1]) || undefined;
+  return (pageUrl && pageUrl.split('/')[index]) || undefined;
 }
 
 module.exports = {
+  region: region,
   section: section
 };
