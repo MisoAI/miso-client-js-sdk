30b8b112e395774d6a56d7415b0b4b528381ae52
simonpai <simon.pai@gmail.com>
2023-01-13 22:14:39 +0800

    Upgrade rollup part 1: upgrade plugins

--- demo/rollup.util.js
+++ demo/rollup.util.js
@@ -5,7 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const cwd = join(__dirname, 'src/entry');
 export const entries = glob.sync('*.js', { cwd }).map(s => s.substring(0, s.length - 3));
--- packages/client-sdk/rollup.util.js
+++ packages/client-sdk/rollup.util.js
@@ -3,7 +3,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
