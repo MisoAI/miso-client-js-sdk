1d7540661f3bf7e5bc1757f94347df09f09d1632
simonpai <simon.pai@gmail.com>
2023-01-16 16:19:23 +0800

    Extract common doc site features to doc-utils.

--- doc/_data/eleventyComputed.js
+++ doc/_data/eleventyComputed.js
@@ -1,17 +1,5 @@
-function chapter(data) {
-  return urlSegment(data, 0);
-}
-
-function section(data) {
-  return urlSegment(data, 1);
-}
-
-function urlSegment({ page }, index) {
-  const pageUrl = page.url.substring(1);
-  return (pageUrl && pageUrl.split('/')[index]) || undefined;
-}
+const { eleventyComputed } = require('@miso.ai/doc-utils');
 
 module.exports = {
-  chapter: chapter,
-  section: section
+  ...eleventyComputed,
 };
--- doc/util/data.js
+++ doc/util/data.js
@@ -1,103 +1,20 @@
-const { loadYaml, readdir } = require('./file');
+const { join } = require('path');
+const { spec, utils } = require('@miso.ai/doc-utils');
 
-function asArray(value) {
-  return value === undefined ? [] : Array.isArray(value) ? value : [value];
-}
-
-function isMixin(key) {
-  return key && key.charCodeAt(0) === 95; // '_'
-}
-
-function getPropMap(rawProps) {
-  return rawProps.reduce((m, prop) => {
-    m[prop.key] = Object.assign({ name: prop.key }, prop);
-    return m;
-  }, {});
-}
-
-function getMixinGroupMap(rawGroups) {
-  return rawGroups.reduce((m, group) => {
-    if (isMixin(group.key)) { 
-      m[group.key] = group;
-    }
-    return m;
-  }, {});
-}
-
-function unfoldProps(propMap, mixinGroupMap, keys, arr = []) {
-  for (const key of keys) {
-    if (isMixin(key)) {
-      unfoldProps(propMap, mixinGroupMap, mixinGroupMap[key].props, arr);
-    } else {
-      arr.push(propMap[key]);
-    }
-  }
-  return arr;
-}
-
-function getPropGroupMap(file) {
-  const { props, groups } = loadYaml(file);
-
-  const propMap = getPropMap(props);
-  const mixinGroupMap = getMixinGroupMap(groups);
-
-  return Object.freeze(groups.reduce((m, group) => {
-    if (!isMixin(group.key)) {
-      const keys = group.keys || [group.key];
-      const props = unfoldProps(propMap, mixinGroupMap, group.props);
-      for (const key of keys) {
-        m[key] = { key, props };
-      }
-    }
-    return m;
-  }, {}));
-}
+const DATA_DIR = join(__dirname, '../_data');
 
 function getComparisonGroup(file) {
-  let { header, groups } = loadYaml(file);
+  let { header, groups } = utils.readYamlSync(join(DATA_DIR, file));
   groups = groups.reduce((m, { props, ...group }) => {
     props = props.map(({ left, right, ...prop }) => ({
       ...prop,
-      left: asArray(left),
-      right: asArray(right),
+      left: utils.asArray(left),
+      right: utils.asArray(right),
     }));
     m[group.key] = { ...group, props };
     return m;
   }, {});
   return Object.freeze({ header, groups });
-}
-
-
-
-function computeProps() {
-  return Object.freeze({
-    helpers: getPropGroupMap('prop/helpers.yml'), 
-    sdk: getPropGroupMap('prop/sdk.yml'), 
-    'ui:model': getPropGroupMap('prop/ui.model.yml'),
-  });
-}
-
-function computeEvent() {
-  const rawEvents = loadYaml('events.yml');
-  const props = Object.freeze(loadYaml('event-props.yml'));
-
-  function getEventProps(name) {
-    return props.filter((prop) => 
-      (!prop.used_by && !prop.used_by_except) ||
-      (prop.used_by && prop.used_by.includes(name)) ||
-      (prop.used_by_except && !prop.used_by_except.includes(name))
-    );
-  }
-
-  const groups = Object.freeze(rawEvents.map((group) => {
-    return Object.assign({}, group, {
-      events: group.events.map((event) => Object.assign({}, event, {
-        props: getEventProps(event.name)
-      }))
-    });
-  }));
-
-  return Object.freeze({ props, groups });
 }
 
 function computeComparisons() {
@@ -106,35 +23,12 @@ function computeComparisons() {
   });
 }
 
-function computePageMeta(file) {
-  const chapterPath = file.split('.')[0];
-  const chapter = loadYaml(`sitemap/${file}`);
-  const result = {};
-  function addPageInfo({ path, title, desc }) {
-    result[`/${chapterPath}${path || ''}/`] = {
-      title: `${chapter.title} - ${title}`,
-      desc,
-    };
-  }
-  for (const { pages, ...section } of chapter.sections) {
-    addPageInfo(section);
-    for (const page of pages) {
-      addPageInfo(page);
-    }
-  }
-  return result;
-}
-
-function computeAllPageMeta() {
-  return Object.freeze(readdir('sitemap').reduce((acc, file) => ({ ...acc, ...computePageMeta(file) }), {}));
-}
-
 function compute() {
   return Object.freeze({
-    props: computeProps(),
-    event: computeEvent(),
+    props: utils.props.buildDir(join(DATA_DIR, 'prop')),
+    event: spec.event,
     comparisons: computeComparisons(),
-    pageMeta: computeAllPageMeta(),
+    pageMeta: utils.sitemap(join(DATA_DIR, 'sitemap')),
   });
 }
 
