fe9f3095d06c161910317dcf8e2eaebbee5bcd0e
simonpai <simon.pai@gmail.com>
2023-03-04 00:26:22 +0800

    [#67] Refactoring

--- packages/client-sdk-ui/src/widget/cards.js
+++ packages/client-sdk-ui/src/widget/cards.js
@@ -1,35 +1,11 @@
 import CollectionWidget from './collection';
-
-function renderProduct(widget, state, { url, cover_image, title, description, sale_price, original_price, ...product }) {
-  const { className } = widget;
-  const openTag = url ? `<a class="${className}__item-body" href="${url}" target="_blank" rel="noopener">` : `<div class="${className}__item-body">`;
-  const clostTag = url ? `</a>` : `</div>`;
-
-  const img = cover_image ? `<img class="${className}__item-cover-image" src="${cover_image}">` : '';
-  const imgBox = `<div class="${className}__item-cover-image-container">${img}</div>`;
-
-  let infoContent = '';
-  if (title) {
-    infoContent += `<div class="${className}__item-title">${title}</div>`;
-  }
-  if (description) {
-    infoContent += `<div class="${className}__item-desc">${description}</div>`;
-  }
-  const price = sale_price || original_price;
-  if (price) {
-    infoContent += `<hr><div class="${className}__item-price">${price}</div>`;
-    // TODO: enhance this
-  }
-  const infoBox = `<div class="${className}__item-info-container">${infoContent}</div>`;
-
-  return `${openTag}${imgBox}${infoBox}${clostTag}`;
-}
+import { product } from './renderers';
 
 const TYPE = 'cards';
 const DEFAULT_CLASSNAME = 'miso-cards';
 
 const DEFAULT_TEMPLATES = Object.freeze({
-  product: renderProduct,
+  product,
 });
 
 const INHERITED_DEFAULT_TEMPLATES = Object.freeze({
--- packages/client-sdk-ui/src/widget/list.js
+++ packages/client-sdk-ui/src/widget/list.js
@@ -1,29 +1,5 @@
 import CollectionWidget from './collection';
-
-function product(widget, state, { url, cover_image, title, description, sale_price, original_price, ...product }) {
-  const { className } = widget;
-  const openTag = url ? `<a class="${className}__item-body" href="${url}" target="_blank" rel="noopener">` : `<div class="${className}__item-body">`;
-  const clostTag = url ? `</a>` : `</div>`;
-
-  const img = cover_image ? `<img class="${className}__item-cover-image" src="${cover_image}">` : '';
-  const imgBox = `<div class="${className}__item-cover-image-container">${img}</div>`;
-
-  let infoContent = '';
-  if (title) {
-    infoContent += `<div class="${className}__item-title">${title}</div>`;
-  }
-  if (description) {
-    infoContent += `<div class="${className}__item-desc">${description}</div>`;
-  }
-  const price = sale_price || original_price;
-  if (price) {
-    infoContent += `<div class="${className}__item-price">${price}</div>`;
-    // TODO: enhance this
-  }
-  const infoBox = `<div class="${className}__item-info-container">${infoContent}</div>`;
-
-  return `${openTag}${imgBox}${infoBox}${clostTag}`;
-}
+import { product } from './renderers';
 
 const TYPE = 'list';
 const DEFAULT_CLASSNAME = 'miso-list';
