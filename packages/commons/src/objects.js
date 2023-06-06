d21daba2bdad8deef20687ba79e6bf83e69731ba
simonpai <simon.pai@gmail.com>
2023-06-06 13:39:24 +0800

    Fine tune

--- packages/commons/src/objects.js
+++ packages/commons/src/objects.js
@@ -12,12 +12,13 @@ export function trimObj(obj) {
   if (typeof obj !== 'object') {
     return obj;
   }
+  const trimmed = {};
   for (const k in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, k) && obj[k] === undefined) {
-      delete obj[k];
+    if (Object.prototype.hasOwnProperty.call(obj, k) && obj[k] !== undefined) {
+      trimmed[k] = obj[k];
     }
   }
-  return obj;
+  return trimmed;
 }
 
 /**
