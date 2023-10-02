cab7694480df8fdac08090f3668ce187e02235d2
simonpai <simon.pai@gmail.com>
2023-10-02 16:33:00 +0800

    Fix dingbat chars

--- packages/commons/src/chars.js
+++ packages/commons/src/chars.js
@@ -1,5 +1,6 @@
-const CIRCLED_DIGITS = '⓪①②③④⑤⑥⑦⑧⑨';
-const NEGATIVE_CIRCLED_DIGITS = '⓿❶❷❸❹❺❻❼❽❾';
+
+const CIRCLED_DIGITS = '➀➁➂➃➄➅➆➇➈➉';
+const NEGATIVE_CIRCLED_DIGITS = '➊➋➌➍➎➏➐➑➒➓';
 
 export function dingbatChar(index, negative) {
   return (negative ? NEGATIVE_CIRCLED_DIGITS : CIRCLED_DIGITS).charAt(index) || '';
